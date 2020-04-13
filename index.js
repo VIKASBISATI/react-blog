d4398464fbf026bd28d5745bae2a2e5370153b0e
Vikas Bisati <vikas.bisati@happay.in>
2020-04-13 14:53:47 +0530

    setting dev mode

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-import express from "express";
-import mongoose from "mongoose";
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config =require('./config/key')
+const { User } = require("./model/user");
 const app = express();
 
-mongoose.connect("mongodb+srv://VikasBisati:venkatavikas333#@react-blog-v4gqi.mongodb.net/test?retryWrites=true&w=majority", 
-    {useNewUrlParser: true}).then(()=>{
+mongoose.connect(config.mongoURI, 
+    {useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex: true}).then(()=>{
         console.log("MongoDB connected");
     }).catch((err)=>{
-        console.log("Error connecting to the database");
+        console.log("Error connecting to the database",err);
     });
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-
+app.post('/api/user/register',(req,res)=>{
+    const user = new User(req.body);
+    user.save((err,resData)=>{
+        if(err){
+            return res.status(400).json({success: false, err});
+        }
+        return res.status(200).json({success:true, resData});
+    })
+})
 
 app.listen(4000);
--- model/user.js
+++ model/user.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +30,6 @@ const userSchema = mongoose.Schema({
     }
 });
 
-const user = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema)
 
-export default user;
+module.exports = { User };
