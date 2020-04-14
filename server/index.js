2dd1bf92ec3fc67e569025e636cce4265469ca2d
Vikas Bisati <vikas.bisati@happay.in>
2020-04-14 16:45:53 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.get("/", (req, res)=>{
-    res.json({ "hello": "deployent again after adding vars"})
+    res.json({ "hello": "deployent again "})
 })
 app.get('/api/user/auth', auth, (req,res) => {
     res.status(200).json({
