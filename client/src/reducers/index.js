8b7ea6ebb9863c34ddd92811b65a2a4e6c756e09
Vikas Bisati <vikas.bisati@happay.in>
2020-04-15 10:22:27 +0530

    final commit

---
+++ client/src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+import userReducer from './userReducer';
+import chatReducer from './chatReducer';
+
+
+const rootReducer = combineReducers({
+    userReducer,
+    chatReducer,
+})
+
+export default rootReducer;
